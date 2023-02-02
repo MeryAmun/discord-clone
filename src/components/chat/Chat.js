265641bd93ec99a4f26401f33a702f2511eaa543
MeryAmun <chebesiawah@gmail.com>
2023-02-02 03:10:18 +0100

    chat input section

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './chat.css'
 import ChatHeader from './chatHeader/ChatHeader';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 const Chat = () => {
   return (
     <div className='chat'>
         <ChatHeader/>
+        <div className="chat__messages"></div>
+        <div className="chat__input">
+   <AddCircleIcon fontSize='large'/>
+   <form>
+    <input type="text" placeholder='Message #TestChannel' />
+    <button type='submit' className='chat__inputButton'>Send Message</button>
+   </form>
+   <div className="chat__inputIcons">
+<CardGiftcardIcon fontSize='large'/>
+<GifIcon fontSize='large'/>
+<EmojiEmotionsIcon fontSize='large'/>
+   </div>
+        </div>
     </div>
   )
 }
